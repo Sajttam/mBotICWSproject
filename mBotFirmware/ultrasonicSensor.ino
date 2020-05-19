0e0a3fce070e78eb1435d52e62952f6f3e5f862a
Eric <rylle98@hotmail.se>
2020-05-19 13:19:59 +0200

    fixed a commit error

--- mBotFirmware/ultrasonicSensor.ino
+++ mBotFirmware/ultrasonicSensor.ino
@@ -1,21 +1,9 @@
 /*
  * ultraSonic:
-<<<<<<< Updated upstream
- * reads 
- * from sensor, checks if there is obstacles within 15 cm
- * if obstacle send BT MSG
- */
-void ultraSonic(){
-  dist = ultrasensor.distanceCm();
-  if(dist >= 0 && dist < 15 && dist != olddist){
-    olddist = dist;
-    //sendJsonMSG();
-  }
-=======
  * reads from sensor,
  * save value, to be sent with next update msg
+ * 
  */
 void ultraSonic(){
   dist = ultrasensor.distanceCm();
->>>>>>> Stashed changes
 }
